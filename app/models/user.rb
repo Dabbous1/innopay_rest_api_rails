d720bf4d6f1914dd51344f851959d4fea19f9259
Aly Dabbous <alydabbous1@gmail.com>
2018-08-16 11:17:13 +0200

    removing password_confirmation from users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ApplicationRecord
+
+    has_secure_password 
+    validates_confirmation_of :password 
+    validates_presence_of  :password_digest
+    
     validates_presence_of :email
     validates_uniqueness_of :email, case_sensitive: false
     validates_format_of :email, with: /@/
+
+    before_save :downcase_email
+    
+    def downcase_email
+        self.email = self.email.delete(' ').downcase
+    end
+      
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180815134847) do
+ActiveRecord::Schema.define(version: 20180816090208) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", null: false
     t.string "password_digest", null: false
-    t.string "password_confirmation", null: false
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
